73fd8d1050bb0a7766313b24071e5654087c8c07
Vladimir Ignatov <vlad.ignatov@gmail.com>
2020-01-10 16:35:43 -0500

    Initial commit

---
+++ src/QueryBuilder.js
@@ -0,0 +1,13 @@
+
+export default class QueryBuilder
+{
+    
+}
+
+export function getAllPatients() {
+    // First get all patients with as much info as we can get for them in a
+    // convenient way
+    const sql = "SELECT * FROM Patient LIMIT 100"; 
+}
+
+
